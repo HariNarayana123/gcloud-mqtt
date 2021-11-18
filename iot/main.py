0bc9a591888292227eee12be631958f1865ffa21
sandeep3028 <35072657+sandeep3028@users.noreply.github.com>
2021-11-18 08:11:46 -0500

    adding main.py for cloud function.
    adding main.py for cloud function.

---
+++ iot/main.py
@@ -0,0 +1,13 @@
+import base64
+import json
+import os
+from google.cloud import iot_v1
+
+
+def subscribe(event, context):
+    data = base64.b64decode(event['data'])
+    print(f'data in function {data}')
+    client = iot_v1.DeviceManagerClient()
+    device_path = client.device_path("halogen-byte-329812","us-central1","my-python-registry","my-python-device")
+    if int(data) > 50:
+        client.send_command_to_device(request={"name":device_path,"binary_data":data})
