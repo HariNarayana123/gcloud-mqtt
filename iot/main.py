41a3da113c534a3a9a4e7e025e4f892f413515b6
sandeep3028 <35072657+sandeep3028@users.noreply.github.com>
2021-11-19 07:52:16 -0500

    device name changed
    device name changed

--- iot/main.py
+++ iot/main.py
@@ -8,6 +8,6 @@ def subscribe(event, context):
     data = base64.b64decode(event['data'])
     print(f'data in function {data}')
     client = iot_v1.DeviceManagerClient()
-    device_path = client.device_path("halogen-byte-329812","us-central1","temperature-registry","temperature-sensor-device")
+    device_path = client.device_path("halogen-byte-329812","us-central1","temperature-registry","temperature-fan-device")
     if int(data) > 50:
         client.send_command_to_device(request={"name":device_path,"binary_data":data})
