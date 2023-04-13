0c8d6b14440f91a1fc713eca748a224d2e5f5561
HariNarayana123 <62258694+HariNarayana123@users.noreply.github.com>
2023-04-13 10:07:32 +0530

    Update mqtt_consumer_demo.py

--- iot/mqtt_consumer_demo.py
+++ iot/mqtt_consumer_demo.py
@@ -5,6 +5,8 @@ import time
 import jwt
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
+from google.cloud import iot_v1
+import base64
 
 connected = False
 messagerecieved = False;
@@ -17,6 +19,40 @@ mqtt_bridge_port = 443
 jwt_iat = datetime.datetime.utcnow()
 jwt_exp_mins = 20
 
+parser = argparse.ArgumentParser(description=("Arg Parse"))
+parser.add_argument("--project_id", required=True)
+parser.add_argument("--cloud_region", required=True)
+parser.add_argument("--registry_id", required=True)
+parser.add_argument("--device_id", required=True)
+args = parser.parse_args()
+
+project_id = args.project_id
+cloud_region = args.cloud_region
+registry_id = args.registry_id
+device_id = args.device_id
+
+def decrypt_payload(encrypted_payload):
+    # Initialize the IoT Core client
+    client = iot_v1.DeviceManagerClient()
+
+    # Construct the fully-qualified device path
+    device_path = client.device_path(project_id, cloud_region, registry_id, device_id)
+
+    # Decode the encrypted payload from base64
+    decoded_payload = base64.b64decode(encrypted_payload)
+
+    # Construct the decrypt request
+    decrypt_request = iot_v1.DecryptDeviceRequest(
+        name=device_path,
+        ciphertext=decoded_payload,
+    )
+
+    # Decrypt the payload using the IoT Core client
+    decrypted_payload = client.decrypt_device(decrypt_request).plaintext
+
+    # Convert the decrypted payload from bytes to string
+    return decrypted_payload.decode("utf-8")
+
 
 def on_connect(unused_client, unused_userdata, unused_flags, rc):
     """Callback for when a device connects."""
@@ -24,14 +60,25 @@ def on_connect(unused_client, unused_userdata, unused_flags, rc):
     global connected
     connected = True
 
+#def on_message(unused_client, unused_userdata, message):
+   # """Callback when the device receives a message on a subscription."""
+ #   payload = str(message.payload.decode("utf-8"))
+  #  print(
+   #     "Received message '{}' on topic '{}' with Qos {}".format(
+   #         payload, message.topic, str(message.qos)
+  #      )
+    #)
+ 
 def on_message(unused_client, unused_userdata, message):
-    """Callback when the device receives a message on a subscription."""
-    payload = str(message.payload.decode("utf-8"))
-    print(
-        "Received message '{}' on topic '{}' with Qos {}".format(
-            payload, message.topic, str(message.qos)
-        )
-    )
+    # Extract the encrypted payload from the message
+    encrypted_payload = message.payload
+
+    # Decrypt the payload 
+    decrypted_payload = decrypt_payload(encrypted_payload)
+
+    # Print the decrypted payload
+    print("Received payload: {}".format(decrypted_payload))
+
     
 
 def create_jwt(project_id, private_key_file, algorithm):
