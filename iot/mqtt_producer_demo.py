dbb1dead64f5d5d62efe783e832e0f4674266b94
HariNarayana123 <62258694+HariNarayana123@users.noreply.github.com>
2023-04-14 12:19:05 +0530

    Update mqtt_producer_demo.py

--- iot/mqtt_producer_demo.py
+++ iot/mqtt_producer_demo.py
@@ -18,6 +18,14 @@ mqtt_bridge_port = 443
 jwt_iat = datetime.datetime.utcnow()
 jwt_exp_mins = 20
 
+Spi=spidev.SpiDev()
+Spi.open(0,0)
+
+Spi.mode=0
+Spi.max_speed_hz=1350000
+vref=3.3
+
+
 
 def on_connect(unused_client, unused_userdata, unused_flags, rc):
     """Callback for when a device connects."""
@@ -52,14 +60,20 @@ def create_jwt(project_id, private_key_file, algorithm):
 
     return jwt.encode(token, private_key, algorithm=algorithm)
 
+def read_adc(channel):
+    adc= Spi.xfer2([1,(8+channel)<<4,0])
+    data=((adc[1] & 0x03) << 8) + adc[2]
+    voltage = (data * vref) / 1023.0
+    return round(voltage,2)
+
 def get_data()
-  
-    
-    
+
+    soil_moisture =read_adc(1)
+    rainfall = read_adc(0)
     
     data ={
-        "soil_moisture" :
-        "rainfall":  
+        "soil_moisture" :soil_moisture,
+        "rainfall":rainfall
     }
     return json.dumps(data)
     
@@ -68,14 +82,13 @@ parser.add_argument("--project_id", required=True)
 parser.add_argument("--cloud_region", required=True)
 parser.add_argument("--registry_id", required=True)
 parser.add_argument("--device_id", required=True)
-parser.add_argument("--temperature", required=True)
 args = parser.parse_args()
 
 project_id = args.project_id
 cloud_region = args.cloud_region
 registry_id = args.registry_id
 device_id = args.device_id
-payload = args.temperature
+payload = get_data()
 
 client_id = "projects/{}/locations/{}/registries/{}/devices/{}".format(
         project_id, cloud_region, registry_id, device_id
