c49eb745138fba6e55fa252c1bc770d6d2c14a66
HariNarayana123 <62258694+HariNarayana123@users.noreply.github.com>
2023-04-14 11:31:15 +0530

    Update mqtt_consumer_demo.py

--- iot/mqtt_consumer_demo.py
+++ iot/mqtt_consumer_demo.py
@@ -31,27 +31,6 @@ cloud_region = args.cloud_region
 registry_id = args.registry_id
 device_id = args.device_id
 
-def decrypt_payload(encrypted_payload):
-    # Initialize the IoT Core client
-    client = iot_v1.DeviceManagerClient()
-
-    # Construct the fully-qualified device path
-    device_path = client.device_path(project_id, cloud_region, registry_id, device_id)
-
-    # Decode the encrypted payload from base64
-    decoded_payload = base64.b64decode(encrypted_payload)
-
-    # Construct the decrypt request
-    decrypt_request = iot_v1.DecryptDeviceRequest(
-        name=device_path,
-        ciphertext=decoded_payload,
-    )
-
-    # Decrypt the payload using the IoT Core client
-    decrypted_payload = client.decrypt_device(decrypt_request).plaintext
-
-    # Convert the decrypted payload from bytes to string
-    return decrypted_payload.decode("utf-8")
 
 
 def on_connect(unused_client, unused_userdata, unused_flags, rc):
@@ -60,26 +39,15 @@ def on_connect(unused_client, unused_userdata, unused_flags, rc):
     global connected
     connected = True
 
-#def on_message(unused_client, unused_userdata, message):
-   # """Callback when the device receives a message on a subscription."""
- #   payload = str(message.payload.decode("utf-8"))
-  #  print(
-   #     "Received message '{}' on topic '{}' with Qos {}".format(
-   #         payload, message.topic, str(message.qos)
-  #      )
-    #)
- 
 def on_message(unused_client, unused_userdata, message):
-    # Extract the encrypted payload from the message
-    encrypted_payload = message.payload
-
-    # Decrypt the payload 
-    decrypted_payload = decrypt_payload(encrypted_payload)
-
-    # Print the decrypted payload
-    print("Received payload: {}".format(decrypted_payload))
-
-    
+    """Callback when the device receives a message on a subscription."""
+    payload = str(message.payload.decode("utf-8"))
+    print(
+        "Received message '{}' on topic '{}' with Qos {}".format(
+            payload, message.topic, str(message.qos)
+        )
+    )
+ 
 
 def create_jwt(project_id, private_key_file, algorithm):
 
@@ -105,24 +73,13 @@ def create_jwt(project_id, private_key_file, algorithm):
     return jwt.encode(token, private_key, algorithm=algorithm)
 
 
-parser = argparse.ArgumentParser(description=("Arg Parse"))
-parser.add_argument("--project_id", required=True)
-parser.add_argument("--cloud_region", required=True)
-parser.add_argument("--registry_id", required=True)
-parser.add_argument("--device_id", required=True)
-args = parser.parse_args()
-
-project_id = args.project_id
-cloud_region = args.cloud_region
-registry_id = args.registry_id
-device_id = args.device_id
-
 client_id = "projects/{}/locations/{}/registries/{}/devices/{}".format(
         project_id, cloud_region, registry_id, device_id
     )
+
 topic = "/devices/{}/{}".format(device_id, "events")
 #topic = "projects/halogen-byte-329812/topics/my-python-topic"
-mqtt_command_topic = "/devices/{}/commands/#".format(device_id)
+#mqtt_command_topic = "/devices/{}/commands/#".format(device_id)
 #mqtt_config_topic = "/devices/{}/config".format(device_id)
 
 print("Client Id : {}\nTopic : {}\n".format(client_id, topic))
@@ -133,14 +90,13 @@ client.username_pw_set(
         username="unused", password=create_jwt(project_id, private_key_file, algorithm)
     )
 client.tls_set(ca_certs=ca_certs, tls_version=ssl.PROTOCOL_TLSv1_2)
+
 client.on_connect = on_connect
 
 
 client.connect(mqtt_bridge_hostname, mqtt_bridge_port)
 client.loop_start()
 print("loop start")
-
-client.subscribe(mqtt_command_topic)
 
 while connected != True:
     time.sleep(0.3)
